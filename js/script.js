9dcf5fb6e336537337ce1a19e30495fac154c053
maTe966 <101216123+maTe966v2@users.noreply.github.com>
2022-04-04 08:45:58 +0200

    add getJSON

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
+// $(function () {
+//     let user = {
+//         userId: "",
+//         id: "",
+//         title: "",
+//         body: "",
+//     };
+
+//     const createUser = () => {
+//         $(".user").html(
+//             `<p>User ID: ${user.userId}</p><p>ID: ${user.id}</p><p>title: ${user.title}</p><p>body: ${user.body}</p>`
+//         );
+//     };
+
+//     $(".get-data").click(function () {
+//         $.get("https://akademia108.pl/api/ajax/get-post.php", function (data) {
+//             user.userId = data.userId;
+//             user.id = data.id;
+//             user.title = data.title;
+//             user.body = data.body;
+//             console.log(data);
+//             createUser();
+//         });
+//     });
+// });
+
 $(function () {
     let user = {
         userId: "",
@@ -13,13 +39,16 @@ $(function () {
     };
 
     $(".get-data").click(function () {
-        $.get("https://akademia108.pl/api/ajax/get-post.php", function (data) {
-            user.userId = data.userId;
-            user.id = data.id;
-            user.title = data.title;
-            user.body = data.body;
+        $.getJSON(
+            "https://akademia108.pl/api/ajax/get-post.php",
+            function (data) {
+                user.userId = data.userId;
+                user.id = data.id;
+                user.title = data.title;
+                user.body = data.body;
 
-            createUser();
-        });
+                createUser();
+            }
+        );
     });
 });
