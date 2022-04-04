346c69029f69363e279ee9c238ff4ef815f1b3ea
maTe966 <101216123+maTe966v2@users.noreply.github.com>
2022-04-04 08:41:42 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(function () {
+    let user = {
+        userId: "",
+        id: "",
+        title: "",
+        body: "",
+    };
+
+    const createUser = () => {
+        $(".user").html(
+            `<p>User ID: ${user.userId}</p><p>ID: ${user.id}</p><p>title: ${user.title}</p><p>body: ${user.body}</p>`
+        );
+    };
+
+    $(".get-data").click(function () {
+        $.get("https://akademia108.pl/api/ajax/get-post.php", function (data) {
+            user.userId = data.userId;
+            user.id = data.id;
+            user.title = data.title;
+            user.body = data.body;
+
+            createUser();
+        });
+    });
+});
